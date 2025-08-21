23dff4c40cd5dde283831c123764c783c654b74c
prathameshLodekar <prathameshlodekar.com>
2025-08-21 18:34:49 +0530

    toggle

--- src/main/resources/static/js/toggle.js
+++ src/main/resources/static/js/toggle.js
@@ -1,4 +1,28 @@
 
+
+function toggleForm() {
+    const loginBox = document.getElementById("login-box");
+    const registerBox = document.getElementById("register-box");
+    const formTitle = document.getElementById("formTitle");
+    // const toggleBtn = document.querySelector("button[onclick='toggleForm()']")
+    const change = document.getElementById("change");
+
+
+    const isLoginVisible = !loginBox.classList.contains("hidden");
+
+    if (isLoginVisible) {
+        loginBox.classList.add("hidden");
+        registerBox.classList.remove("hidden");
+        formTitle.innerText = "Register";
+        change.style.display = "none";
+    } else {
+        loginBox.classList.remove("hidden");
+        registerBox.classList.add("hidden");
+        formTitle.innerText = "Login";
+        change.style.display = "flex";
+    }
+}
+
 function checkPassword(){
     const pass = document.getElementById("password");
     const confirm = document.getElementById("confirmPassword");
